167ca6fb7bba35c30e6998291931c6f7a48a2094
Lucas Rocha <lucas_rocha.14@outlook.com>
2025-12-26 12:04:01 -0300

    add best practices of security

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,9 +4,31 @@ import { AppService } from './app.service';
 import { IdeiaController } from './routes/idea/ideia.controller';
 import { OpenaiService } from './routes/idea/openai.service';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'short',
+          ttl: 60,
+          limit: 10,
+        },
+        {
+          name: 'medium',
+          ttl: 300,
+          limit: 20,
+        },
+        {
+          name: 'long',
+          ttl: 600,
+          limit: 100,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController, IdeiaController],
   providers: [AppService, OpenaiService],
 })
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3333, '0.0.0.0');
 }

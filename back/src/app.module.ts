486676bfd474c6ae703f496c0cf7b380130bfbcf
Lucas Rocha <lucas_rocha.14@outlook.com>
2025-12-26 11:44:34 -0300

    restructuring files

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { IdeiaController } from './idea/ideia.controller';
-import { OpenaiService } from './idea/openai.service';
+import { IdeiaController } from './routes/idea/ideia.controller';
+import { OpenaiService } from './routes/idea/openai.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
